0c6dc11140c2afc2032441d81bb249a2f35229b0
김재령 <wofud0321@naver.com>
2023-04-12 15:39:26 +0900

    회원가입까지 설계

---
+++ src/main/java/jaeboard/board/service/MemberService.java
@@ -0,0 +1,36 @@
+package jaeboard.board.service;
+
+import jaeboard.board.entity.Member;
+import jaeboard.board.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public Long Join(Member member) {
+        validateDuplicateMember(member);    // 중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getUsername());
+
+        if(!findMembers.isEmpty()){
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
+
+}
---
+++ src/test/java/jaeboard/board/service/MemberServiceTest.java
@@ -0,0 +1,36 @@
+package jaeboard.board.service;
+
+import jaeboard.board.entity.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    void join() {
+        Member member1 = new Member();
+        Member member2 = new Member();
+        member1.setUsername("김재령");
+        member2.setUsername("김재령");
+
+        Long member1Id = memberService.Join(member1);
+        // Long member2Id = memberService.Join(member2); 중복회원 에러 발생유도
+
+        assertThat(member1Id).isEqualTo(member1.getId());
+    }
+}
